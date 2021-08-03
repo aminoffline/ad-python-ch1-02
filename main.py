07a6c5f60a3519bb8cfb08b5bc9f421cb348cb38
Amin <aminoffline@yahoo.com>
2021-08-04 01:22:39 +0430

    initial - just creating the dics

---
+++ main.py
@@ -0,0 +1,8 @@
+Spain = {wins:0 , loses:0 , draws:0 , goal difference:0 , points:0}
+Iran = {wins:0 , loses:0 , draws:0 , goal difference:0 , points:0}
+Portugal = {wins:0 , loses:0 , draws:0 , goal difference:0 , points:0}
+Morocco = {wins:0 , loses:0 , draws:0 , goal difference:0 , points:0}
+
+
+
+
