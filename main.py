e88dadc1e4056da94074f186a14d6522363ad9c4
Amin <aminoffline@yahoo.com>
2021-08-04 01:24:13 +0430

    initial - it dosen't work

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-Spain = {wins:0 , loses:0 , draws:0 , goal difference:0 , points:0}
-Iran = {wins:0 , loses:0 , draws:0 , goal difference:0 , points:0}
-Portugal = {wins:0 , loses:0 , draws:0 , goal difference:0 , points:0}
-Morocco = {wins:0 , loses:0 , draws:0 , goal difference:0 , points:0}
+for team in ['Spain' , 'Iran' ,'Portugal' , 'Morocco']:
+    team = {"wins":0 , "loses":0 , "draws":0 , "goal difference":0 , "points":0 }
+print(Morocco , Portugal,Iran,Spain) 
+
+def dic_creation(team):
+    team = {"wins":0 , "loses":0 , "draws":0 , "goal difference":0 , "points":0 }
+    return team
 
 
-
-
+class Group_B:
+    def f_status(self):
+        self = {"wins": 0, "loses": 0, "draws": 0, "goal difference": 0, "points": 0}
+        return self
+group_b = Group_B
+Spain = group_b.f_status
+print(Spain.items)
